f1470639367ae47f0e43401b94beb5afef4abcbf
Stefan Schustereit <37613765+sschuste@users.noreply.github.com>
2021-04-07 14:47:25 +0200

    Update nanodmx_driver.js

--- nanodmx_driver.js
+++ nanodmx_driver.js
@@ -2,61 +2,50 @@
 
 var SerialPort = require("serialport")
 
-var	ENTTEC_PRO_DMX_STARTCODE = 0x00,
-	ENTTEC_PRO_START_OF_MSG  = 0x7e,
-	ENTTEC_PRO_END_OF_MSG    = 0xe7,
-	ENTTEC_PRO_SEND_DMX_RQ   = 0x06,
-	ENTTEC_PRO_RECV_DMX_PKT  = 0x05;
+var UNIVERSE_LEN = 512;
 
 function DMX(device_id, current_universe) {
-	console.log("initalizing dmx usb pro...");
+	console.log("initalizing NanoDMX USB interface...");
 	var self = this
 	this.universe = current_universe;
 
 	this.dev = new SerialPort(device_id, {
-		'baudRate': 250000,
+		'baudRate': 38400,
 		'databits': 8,
-		'stopbits': 2,
+		'stopbits': 1,
 		'parity': 'none'
 	}, function(err) {
 		if(err != null){
-			console.log("dmx enttec driver device error:" + err);
+			console.log("NanoDMX driver device error:" + err);
 		}
 	});
 }
 
 DMX.prototype.send_universe = function() {
-	var hdr = Buffer([
-		ENTTEC_PRO_START_OF_MSG,
-		ENTTEC_PRO_SEND_DMX_RQ,
-		 (this.universe.length + 1)       & 0xff,
-		((this.universe.length + 1) >> 8) & 0xff,
-		ENTTEC_PRO_DMX_STARTCODE
-	])
-	var msg = Buffer.concat([
-		hdr,
-		this.universe,
-		Buffer([ENTTEC_PRO_END_OF_MSG])
-	])
+	const msg = Buffer.alloc(UNIVERSE_LEN * 3);
+
+	for (let i = 0; i < UNIVERSE_LEN; i++) {
+		msg[i * 3 + 0] = (i < 256) ? 0xE2 : 0xE3;
+		msg[i * 3 + 1] = i;
+		msg[i * 3 + 2] = this.universe[i + 1];
+	}
 	this.dev.write(msg)
 }
 
 DMX.prototype.close = function(cb) {
-	console.log("closing DMX USB PRO device... " + cb);
+	console.log("closing NanoDMX device... " + cb);
 	this.dev.close(cb)
 }
 
 DMX.prototype.update = function(u, offset) {
 	for(var c in u) {
-		this.universe[(parseFloat(c)+parseFloat(offset)-parseFloat(1))] = u[c];
-		//this.universe[(parseFloat(c)+parseFloat(offset)-parseFloat(2))] = u[c];
-		//console.log("c: " + c + " u " + JSON.stringify(u) + " u[c] " + u[c]);
+		this.universe[c] = u[c];
 	}
 	this.send_universe()
 }
 
 DMX.prototype.updateAll = function(v) {
-	for(var i = 0; i < 512; i++) {
+	for(var i = 1; i <= 512; i++) {
 		this.universe[i] = v
 	}
 	this.send_universe()
@@ -64,7 +53,7 @@ DMX.prototype.updateAll = function(v) {
 
 DMX.prototype.universeToObject = function() {
 	var u = {}
-	for(var i = 0; i < this.universe.length; i++) {
+	for(var i = 1; i <= this.universe.length; i++) {
 		u[i] = this.get(i)
 	}
 	return u
@@ -75,4 +64,3 @@ DMX.prototype.get = function(c) {
 }
 
 module.exports = DMX
- 
