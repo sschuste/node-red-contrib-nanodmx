1b82729cfda6e41cdfd0bf3e45bc9daa57bb81a2
Stefan Schustereit <37613765+sschuste@users.noreply.github.com>
2024-08-07 13:46:46 +0200

    Update dmx4all.js

--- dmx4all.js
+++ dmx4all.js
@@ -1,35 +1,66 @@
 module.exports = function(RED) {
-        "use strict";
-        var DMX = require('./dmx4all_driver.js');
-        function DMXout(options) {
-                RED.nodes.createNode(this,options);
-                // console.log("this in DMXout: " + JSON.stringify(this));
-                var node        = this;
-                this.port       = options.port || "COM3";
-                this.dmxOffset  = options.dmxStartingAddress || 1;
+    "use strict";
+    var DMX = require('./nanodmx_driver.js');
 
-                var current_universe_buffer = new Buffer(512);
+    function DMXout(config) {
+        RED.nodes.createNode(this, config);
+        var node = this;
 
-                var current_universe = [];
-                for (var i = 0; i<512; i++){
-                        current_universe[i] = 0;
-                }
-                current_universe_buffer = Buffer(current_universe);
+        this.configNode = RED.nodes.getNode(config.config);
+        if (this.configNode) {
+            this.port = this.configNode.port;
+            this.dmxOffset = this.configNode.dmxStartingAddress;
+            this.baudrate = this.configNode.baudrate;
+            this.DmxUsbDevice = this.configNode.DmxUsbDevice;
+        } else {
+            this.error("No configuration node found");
+            return;
+        }
 
-                var DmxUsbDevice = new DMX(this.port, current_universe_buffer);
+        var currentUniverse = [];
 
-                node.on('input',function(msg) {
-                        current_universe = msg.payload;
-                        // console.log(current_universe);
-                        DmxUsbDevice.update(current_universe, this.dmxOffset);
-                        node.send(msg);
-                });
-                this.on('close', function(done) {
-                        DmxUsbDevice.close(function (err) {
-                            console.log('[info] DMX4ALL USB device closed', err);
-                            done();
-                        });
-                });
+        node.on('input', function(msg) {
+            var input = Object.keys(msg.payload).length;
+            if (input) {
+                currentUniverse = msg.payload;
+                node.DmxUsbDevice.update(currentUniverse, node.dmxOffset);
+            } else {
+                msg.payload = node.DmxUsbDevice.universeToObject();
+            }
+            node.send(msg);
+        });
+
+        this.on('close', function(done) {
+            // Nothing to do here, as the port is managed by the config node
+            done();
+        });
     }
-    RED.nodes.registerType("dmx4all",DMXout);
+    RED.nodes.registerType("nanodmx", DMXout);
+
+    function DMXConfigNode(n) {
+        RED.nodes.createNode(this, n);
+        this.port = n.port;
+        this.dmxStartingAddress = n.dmxStartingAddress;
+        this.baudrate = n.baudrate;
+        this.databits = 8;
+        this.stopbits = 1;
+        this.parity = "none";
+
+        if (!this.DmxUsbDevice) {
+            var currentUniverseBuffer = Buffer.alloc(512);
+            this.DmxUsbDevice = new DMX(this.port, this.baudrate, this.databits, this.stopbits, this.parity, currentUniverseBuffer);
+        }
+
+        this.on('close', function(done) {
+            if (this.DmxUsbDevice) {
+                this.DmxUsbDevice.close(function(err) {
+                    console.log('[info] DMX4ALL NanoDMX USB device closed', err);
+                    done();
+                });
+            } else {
+                done();
+            }
+        });
+    }
+    RED.nodes.registerType("nanodmx-config", DMXConfigNode);
 }
